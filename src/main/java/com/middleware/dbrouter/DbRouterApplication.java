4568f5b0c1a70473461b376502767acd78c1d013
Curtis-Mou <1337913241@qq.com>
2024-12-10 14:06:01 +0800

    annotation模块放自定义的注解

---
+++ src/main/java/com/middleware/dbrouter/DbRouterApplication.java
@@ -0,0 +1,13 @@
+package com.middleware.dbrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbRouterApplication.class, args);
+    }
+
+}
